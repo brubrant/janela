c09b4e6f52bfe849bfe3e8bbae96fec5405d77be
Eduardo Zulian <eduardozulian@gmail.com>
2011-11-29 19:39:20 -0200

    minor fixes.

--- author.php
+++ author.php
@@ -2,19 +2,14 @@
 
 	<section id="content">
 
-		<?php 
-            
-        $member = get_userdata( get_query_var( 'author' ) );
-        
-        print_r($member);
-        ?>
+		<?php $member = get_userdata( get_query_var( 'author' ) ); ?>
             
         <div class="member-avatar"><?php echo get_avatar( $member->ID, 256, '', 'avatar-' . $member->display_name ); ?></div>
         
         <h1 class="member-name"><?php echo $member->display_name; ?></h1>
         
 		<?php if ( ! empty ( $member->user_url ) ) : ?>
-        <div class="member-url"><?php echo $member->user_url; ?></div>
+        <div class="member-url"><a href="<?php echo esc_url( $member->user_url ); ?>" title=""><?php echo $member->user_url; ?></a></div>
         <?php endif; ?>
         
 		<?php if ( ! empty ( $member->description ) ) : ?>
@@ -25,11 +20,11 @@
         <div class="member-social">
         	<ul>
 				<?php if ( ! empty ( $member->facebook ) ) : ?>
-                <li class="member-facebook"><?php echo $member->facebook; ?></li>
+                <li class="member-facebook"><a href="<?php echo esc_url( $member->facebook ); ?>" title=""><?php echo $member->facebook; ?></a></li>
                 <?php endif; ?>
                 
                 <?php if ( ! empty ( $member->twitter ) ) : ?>
-                <li class="member-twitter"><?php echo $member->twitter; ?></li>
+                <li class="member-twitter"><a href="<?php echo esc_url( $member->twitter ); ?>" title=""><?php echo $member->twitter; ?></a></li>
                 <?php endif; ?>
             </ul>
 		</div><!-- .member-social -->
