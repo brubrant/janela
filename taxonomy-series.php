c49c1467b628b8f1d21cab8c47a2c8cd4e4ed43a
Eduardo Zulian <eduardozulian@gmail.com>
2011-11-30 17:26:48 -0200

    added description.

---
+++ taxonomy-series.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+	<?php the_post(); ?>
+		
+	<header class="archive-header" id="series-header">
+	       
+	    <h2 class="header-title" id="series-title"><?php single_term_title(); ?></h2>			
+	    
+	    <?php
+	    
+	    // Descrição
+	    $series_description = term_description();
+	    
+	    if ( ! empty( $series_description ) ) : ?>
+            <div class="series-description">
+                <?php echo $series_description; ?>
+            </div>
+        <?php
+        endif;
+	    ?>
+	</header><!-- /archive-header -->
+	
+	<?php rewind_posts(); ?>
+
+	<section class="content clearfix" id="content-list">
+		
+		<?php get_template_part( 'loop', 'archive' ); ?>
+
+	</section><!-- /content -->
+
+<?php get_footer(); ?>
