92affd96bf26a9e1e4df3e137ee2de3e43b6ae64
Eduardo Zulian <eduardozulian@gmail.com>
2011-12-01 13:49:03 -0200

    custom fields conditionals added.

--- single.php
+++ single.php
@@ -17,36 +17,36 @@
             	<div id="credit-main">
 			    	<div class="entry-cast">
 			    		<?php $credits = get_post_meta( $post->ID, 'janela_cast', $single = true ); ?>
-			    		<h3>Com</h3>
-			    		<?php $credit = explode(",", $credits); ?>
-			    		<?php foreach($credit as $user_login) : ?>
-			    		<?php $user = get_userdatabylogin($user_login); ?>
-			    		
-			    		<a href="<?php echo get_author_posts_url( $user->ID ); ?>" title="<?php echo $user->display_name; ?>" data-tooltip="<?php echo $user->display_name; ?>"><?php echo get_avatar( $user->ID, 60, 'Nome', false ); ?></a>
-			    	
-			    		<?php endforeach; ?>
+                        <?php if ( ! empty ( $credits ) ) : ?>
+                            <h3>Com</h3>
+                            <?php $credit = explode(",", $credits); ?>
+                            <?php foreach($credit as $user_login) : ?>
+                            	<?php $user = get_userdatabylogin($user_login); ?>
+			    				<a href="<?php echo get_author_posts_url( $user->ID ); ?>" title="<?php echo $user->display_name; ?>" data-tooltip="<?php echo $user->display_name; ?>"><?php echo get_avatar( $user->ID, 60, 'Nome', false ); ?></a>
+			    			<?php endforeach; ?>
+						<?php endif; ?>
 			    	</div>
 			    	<div class="entry-directed">
 			    		<?php $credits = get_post_meta( $post->ID, 'janela_directed', $single = true ); ?>
-			    		<h3>Direção</h3>
-			    		<?php $credit = explode(",", $credits); ?>
-			    		<?php foreach($credit as $user_login) : ?>
-			    		<?php $user = get_userdatabylogin($user_login); ?>
-			    		
-			    		<a href="<?php echo get_author_posts_url( $user->ID ); ?>" title="<?php echo $user->display_name; ?>" data-tooltip="<?php echo $user->display_name; ?>"><?php echo get_avatar( $user->ID, 60, 'Nome', false ); ?></a>
-			    	
-			    		<?php endforeach; ?>
+			    		<?php if ( ! empty ( $credits ) ) : ?>
+                            <h3>Direção</h3>
+                            <?php $credit = explode(",", $credits); ?>
+                            <?php foreach($credit as $user_login) : ?>
+                            	<?php $user = get_userdatabylogin($user_login); ?>
+			    				<a href="<?php echo get_author_posts_url( $user->ID ); ?>" title="<?php echo $user->display_name; ?>" data-tooltip="<?php echo $user->display_name; ?>"><?php echo get_avatar( $user->ID, 60, 'Nome', false ); ?></a>
+			    			<?php endforeach; ?>
+						<?php endif; ?>
 			    	</div>
 			    	<div class="entry-wrote">
 			    		<?php $credits = get_post_meta( $post->ID, 'janela_wrote', $single = true ); ?>
-			    		<h3>Roteiro</h3>
-			    		<?php $credit = explode(",", $credits); ?>
-			    		<?php foreach($credit as $user_login) : ?>
-			    		<?php $user = get_userdatabylogin($user_login); ?>
-			    		
-			    		<a href="<?php echo get_author_posts_url( $user->ID ); ?>" title="<?php echo $user->display_name; ?>" data-tooltip="<?php echo $user->display_name; ?>"><?php echo get_avatar( $user->ID, 60, 'Nome', false ); ?></a>
-			    		
-			    		<?php endforeach; ?>
+			    		<?php if ( ! empty ( $credits ) ) : ?>
+                            <h3>Roteiro</h3>
+                            <?php $credit = explode(",", $credits); ?>
+                            <?php foreach($credit as $user_login) : ?>
+                            	<?php $user = get_userdatabylogin($user_login); ?>
+			    				<a href="<?php echo get_author_posts_url( $user->ID ); ?>" title="<?php echo $user->display_name; ?>" data-tooltip="<?php echo $user->display_name; ?>"><?php echo get_avatar( $user->ID, 60, 'Nome', false ); ?></a>
+			    			<?php endforeach; ?>
+						<?php endif; ?>
 			    	</div>
 			    </div>
 			    <div id="credit-full" class="hide">
